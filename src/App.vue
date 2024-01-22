b7dbc5492294fc753bf7473ce78bb2e646a6f712
Soniamy <marianna.marzullo@gmail.com>
2024-01-22 14:22:20 +0100

    Inizio esercizio: 1.Importazione di axios e di bootstrap. 2.Ho scaricato la foto di YU-GI-OH da internet. 3. Creazione dell'header con l'immagine scaricata

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,27 @@ import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
-    data() {
-        return {
-
-        };
-    },
-    components: {
-        AppHeader,
-        AppMain,
-        AppFooter
-    },  
-    methods: {
-
-    }
-}
+  data() {
+    return {};
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  methods: {},
+};
 </script>
 
 <template>
-    <h1>
-        Mia App
-    </h1>
+  <AppHeader />
 
-    <AppHeader />
+  <AppMain />
 
-    <AppMain />
-
-    <AppFooter />
+  <AppFooter />
 </template>
 
 <style lang="scss">
-@use "assets/scss/main" as *;
-@import "assets/scss/partials/reset";
+@use 'assets/scss/main' as *;
+@import 'assets/scss/partials/reset';
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,21 +1,31 @@
 <script>
 export default {
-    data() {
-        return {
-
-        };
-    },
-    methods: {
-
-    }
-}
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <template>
-    <header>
-        HEADER
-    </header>
+  <header>
+    <div class="d-flex align-items-center">
+      <img class="me-3" src="../../public/logo.png" alt="logo" />
+      <h1>Yu-Gi-Oh Api</h1>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables.scss';
+
+header {
+  height: 80px;
+  background-color: white;
+  padding: 5px;
+
+  img {
+    height: 50px;
+  }
+}
 </style>
