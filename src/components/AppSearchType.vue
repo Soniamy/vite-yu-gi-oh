b057ca306dc9bfd5956f61c5a6db578627ce00b8
Soniamy <marianna.marzullo@gmail.com>
2024-01-22 15:10:21 +0100

    creazione label form-select.

--- src/components/AppSearchType.vue
+++ src/components/AppSearchType.vue
@@ -11,6 +11,18 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="container d-flex align-items-center">
+    <div class="form my-3 mx-2">
+      <select class="form-select p-20 width-form">
+        <option selected>Alien</option>
+      </select>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.width-form {
+  width: 215px;
+}
+</style>
